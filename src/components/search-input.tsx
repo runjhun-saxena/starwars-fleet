0f0a354f85ef02bc69f332e66d2b47b5c988a688
Runjhun saxena <runjhunsaxena6@gmail.com>
2025-08-27 00:48:37 +0530

    Add search, filters, data table

---
+++ src/components/search-input.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+import { useAtom } from 'jotai';
+import { searchAtom } from '@/store/starship';
+import { useDebouncedCallback } from 'use-debounce';
+import { useEffect, useState } from 'react';
+
+export function SearchInput() {
+  const [search, setSearch] = useAtom(searchAtom);
+  const [localValue, setLocalValue] = useState(search);
+  const debouncedSetSearch = useDebouncedCallback((value: string) => {
+    setSearch(value);
+  }, 300);
+
+  useEffect(() => {
+    setLocalValue(search);
+  }, [search]);
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setLocalValue(value);
+    debouncedSetSearch(value);
+  };
+  return (
+    <div className="relative">
+      <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
+      <Input
+        placeholder="Search starships..."
+        value={localValue}
+        onChange={handleChange}
+        className="pl-10"
+      />
+    </div>
+  );
+}
